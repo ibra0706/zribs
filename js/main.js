c3a85e603d68ed3c9cb8f4041717f054071287e8
ibra0706 <91210207+ibra0706@users.noreply.github.com>
2021-10-12 09:58:53 +0200

    ja
    ne

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 const btn = document.querySelector('.uredi');
 const overlay = document.querySelector('.overlay');
 const closeBtn = document.querySelector('.close');
-const checked = document.querySelectorAll('.checked');
-
-
+const checked = document.querySelectorAll('i');
+console.log(checked);
 let x;
 btn.addEventListener('click', e=>{
     overlay.classList.add('yes');
@@ -15,15 +14,20 @@ closeBtn.addEventListener('click', ()=>{
     overlay.classList.remove('yes');
 });
 
-checked.forEach((check) => {
-    check.addEventListener('click', () => {
-      let y = check.classList.contains('fa-check');
-      console.log(y);
-      if (y === true) {
-        check.classList.add('green');
-      }
-      else{
-        check.classList.add('red');
-      }
-    });
-  });
+checked.forEach(check=>{
+  check.addEventListener('click', () =>{
+    console.log('323');
+  })
+})
+
+// checked.forEach((check) => {
+//     check.addEventListener('click', () => {
+//       let y = check.classList.contains('fa-check');
+//       if (y === true) {
+//         check.classList.add('green');
+//       }
+//       else{
+//         check.classList.add('red');
+//       }
+//     });
+//   });
