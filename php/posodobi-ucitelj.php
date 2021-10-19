bbbd1a5b3ab7c160aac1143c83aba517d0ed2152
WarwolfPlayz <74459701+WarwolfPlayz@users.noreply.github.com>
2021-10-19 08:08:40 +0200

    admin-ucitelji-fix

--- php/posodobi-ucitelj.php
+++ php/posodobi-ucitelj.php
@@ -1,3 +1,4 @@
+
 <?php
 // Include config file
 require_once "config.php";
@@ -158,7 +159,7 @@ if(isset($_POST["id"]) && !empty($_POST["id"])){
                         </div>
                         <input type="hidden" name="id" value="<?php echo $id; ?>"/>
                         <input type="submit" class="btn btn-primary" value="Submit">
-                        <a href="index.php" class="btn btn-secondary ml-2">Cancel</a>
+                        <a href="ucitelji.php" class="btn btn-secondary ml-2">Cancel</a>
                     </form>
                 </div>
             </div>        
