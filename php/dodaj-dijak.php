00211490d6ddf7f71858c90c6fd2adc815709fb0
ibra0706 <91210207+ibra0706@users.noreply.github.com>
2021-11-02 01:10:06 +0100

    asijodjfsjoifaijfwoij

--- php/dodaj-dijak.php
+++ php/dodaj-dijak.php
@@ -58,7 +58,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
                 // Records created successfully. Redirect to landing page
-                header("location: ucitelji.php");
+                header("location: dijaki.php");
                 exit();
             } else{
                 echo "Oops! Something went wrong. Please try again later.";
@@ -78,7 +78,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Ustvari učitelja</title>
+    <title>Ustvari dijaka</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
         .wrapper{
--- php/nalogaInfo.php
+++ php/nalogaInfo.php
@@ -69,7 +69,12 @@ if($result = mysqli_query($link, $sql)){
                     echo '<div class="flex">'.'<h1>'.$row['naziv'].'</h1>' . '<h3 class="red" onClick="goBack()">X</h3>'.'</div>'. '<br>';
                     echo '<h2>'.$row['navodila'].'</h2>'. '<br>';
                     echo '<h3>'. 'Rok oddaje: '.$row['datum_rok'].'</h3>' . '<br>';
-                    echo '<iframe src="./nalozi.php" title="nalozi nalogo"></iframe>';
+                    echo '<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" enctype="multipart/form-data">
+                    Izberi datoteko, ki jo boš naložil:
+                    <label for="fileToUpload" class="btn">Izberi datoteko</label>
+                    <input type="file" name="fileToUpload" id="fileToUpload" style="visibility:hidden;">
+                    <input type="submit" value="Naloži" name="submit" class="btn">
+                  </form>';
                     echo '</div>';
             }
         // Free result set
@@ -84,7 +89,64 @@ if($result = mysqli_query($link, $sql)){
 // Close connection
 mysqli_close($link);
 ?>
+<?php
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+// require_once "config.php";
+// $id = $_SESSION['idnalog'];
+$predmet;
+$dijak;
+// $target_dir = "../uploads/".$predmet."/".$dijak."/";
+// $target_dir = "../uploads/".$id;
+$target_dir = "../uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
+// // Check if image file is a actual image or fake image
+// if(isset($_POST["submit"])) {
+//   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+//   if($check !== false) {
+//     echo "File is an image - " . $check["mime"] . ".";
+//     $uploadOk = 1;
+//   } else {
+//     echo "File is not an image.";
+//     $uploadOk = 0;
+//     // Adrian was here
+//   }
+// }
+
+// Check if file already exists
+if (file_exists($target_file)) {
+  echo "Ta datoteka že obstaja";
+  $uploadOk = 0;
+}
+
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 10000000) {
+  echo "Sorry, your file is too large.";
+  $uploadOk = 0;
+}
+
+// Allow certain file formats
+// if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+// && $imageFileType != "gif" ) {
+//   echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+//   $uploadOk = 0;
+// }
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+  }
+}
+}
+?>
 <script>
 function goBack() {
   window.history.back();
