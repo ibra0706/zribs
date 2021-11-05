3be3ac607661a7050c05479ecaaef58c8ab817b9
WarwolfPlayz <74459701+WarwolfPlayz@users.noreply.github.com>
2021-11-05 17:50:17 +0100

    dodaj/brisi predmet dijak

--- php/urediPredmeteOdstrani.php
+++ php/urediPredmeteOdstrani.php
@@ -7,31 +7,27 @@ if(session_id() == '' || !isset($_SESSION) || session_status() === PHP_SESSION_N
 
  
 if($_SERVER["REQUEST_METHOD"] == "POST"){
-    $st = trim($_POST['stevilo']);
         if(!empty($_POST['lang'])) {    
             foreach($_POST['lang'] as $value){
 
-
-                $sql = "INSERT INTO dijakPredmet (id_dijaki, id_predmet) VALUES (?, ?)";
-         
+                $sql = "DELETE FROM dijakPredmet WHERE id_di_pre_povezava = ?";
+    
                 if($stmt = mysqli_prepare($link, $sql)){
-                    mysqli_stmt_bind_param($stmt, "ii", $par_dijak, $par_predmet);
+                    mysqli_stmt_bind_param($stmt, "i", $param_id);
                     
-                    $par_dijak = $id;
-                    $par_predmet = $value;
+                    $param_id = $value;
+                    
                     if(mysqli_stmt_execute($stmt)){
-                        
+
                     } else{
                         echo "Oops! Something went wrong. Please try again later.";
                     }
                 }
-                mysqli_stmt_close($stmt);
-            }
-            // header("location: urediPredmete.php");
-            exit();
-
+        }     
+        mysqli_stmt_close($stmt);
     }
 }
+    
 
 ?>
 <!DOCTYPE html>
@@ -41,21 +37,21 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../css/urediPredmete.css">
 </head>
 <body>
-    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="POST">
-    <?php
+<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="POST">
+<?php
     require_once "config.php";
-    $id = $_SESSION["id"];
+    $id = $_SESSION['id'];
     $stPred = 0;
-    $sql = "SELECT  p.* FROM dijakPredmet d, predmeti p  where id_dijaki = $id AND not p.id_predmet = d.id_predmet;";
+    $sql = "SELECT p.*, d.* FROM dijakPredmet d, predmeti p  where id_dijaki = $id AND  p.id_predmet = d.id_predmet;";
     if($result = mysqli_query($link, $sql)){
         if(mysqli_num_rows($result) > 0){
                 while($row = mysqli_fetch_array($result)){
+                        $stPred++;
                         echo '<div class="predmetDodaj">';
                         echo $row['ime_predmeta'];
-                        echo '<input type="checkbox" name="lang[]" value="'.$row['id_predmet'].'"></div>';
+                        echo '<input type="checkbox" name="lang[]" value="'.$row['id_di_pre_povezava'].'"></div>';
                         echo '<script> console.log("'.$row['id_predmet'].'")</script>';
                 }
             mysqli_free_result($result);
@@ -66,9 +62,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         echo "Oops! Something went wrong. Please try again later.";
     }
     mysqli_close($link);
-    echo '<input type="number" name="stevilo" hidden value="'.$stPred.'">';
     ?>
-    <input type="submit" value="Dodaj">
-    </form>
+    <input type="submit" value="Briši">
 </body>
 </html>
